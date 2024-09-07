097ebaa03614145452ca80aba5d3023e9c577216
sorushkrg <92299927+sorushkrg@users.noreply.github.com>
2024-09-08 00:29:26 +0330

    update card

---
+++ assets/js/master.js
@@ -0,0 +1,28 @@
+const testUrl = "https://fakestoreapi.com/products?limit=12";
+const product = getApi(testUrl);
+async function getApi(url) {
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`data ERROR : ${response.status}`);
+		}
+		return response.json();
+	} catch (error) {
+		console.log("error");
+	}
+}
+
+async function addToHtml() {
+	try {
+		const data = await product;
+		data.map((item, Index) => {
+			const a = $(`<a href="#" class="item d-flex flex-column gap-2"
+                    ><img src="${item.image}" alt="" />
+                    <h2 class="mt-4">${item.title}</h2>
+                    <div class="price">${item.price}$</div></a
+                >`);
+			$(".listProduct").append(a);
+		});
+	} catch (error) {}
+}
+addToHtml();
